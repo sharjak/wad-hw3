9f3d40f516da185963896e822f47bb38f5a6da8f
sharjak <sharjak@gmail.com>
2019-11-09 19:20:20 +0200

    Created AddCourseForm component and toggleForm function

--- src/components/AddCourseForm.vue
+++ src/components/AddCourseForm.vue
@@ -13,7 +13,7 @@
 
 <script>
     export default {
-        name: "CoursesContainer",
+        name: "AddCourseForm",
         data: () => {
             return {
                 showAddCourseForm: false
